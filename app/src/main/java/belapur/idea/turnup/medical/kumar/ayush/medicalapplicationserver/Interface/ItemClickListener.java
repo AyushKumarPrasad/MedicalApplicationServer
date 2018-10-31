4c0a3130cb0668617db7d72740f9984266d02675
Ayush Kumar <ayushkumar@Ayushs-MacBook-Pro.local>
2018-10-31 18:15:34 +0530

    Initial commit

---
+++ app/src/main/java/belapur/idea/turnup/medical/kumar/ayush/medicalapplicationserver/Interface/ItemClickListener.java
@@ -0,0 +1,8 @@
+package belapur.idea.turnup.medical.kumar.ayush.medicalapplicationserver.Interface;
+
+import android.view.View;
+
+public interface ItemClickListener
+{
+    void onClick(View view, int position, boolean isLongClick);
+}
